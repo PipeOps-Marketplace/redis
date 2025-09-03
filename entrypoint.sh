ed544179d1ac926c227af23dbfbc08abf1ed4530
Alex Idowu <alexidowu25@gmail.com>
2025-09-03 18:14:50 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,7 @@ export REDIS_PASSWORD
 export REDIS_MASTER_PASSWORD
 
 # Use envsubst to replace variables from the template and save to a new file
-envsubst < redis.conf.template > /tmp/redis.conf
+envsubst < /tmp/redis.conf.template > /tmp/redis.conf
 
 # Execute the main command
 exec "$@"
