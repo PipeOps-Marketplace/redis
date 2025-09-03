97faf644ae8cbe448f38659403131ba25d2aea64
Alex Idowu <alexidowu25@gmail.com>
2025-09-03 18:00:29 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,8 +2,9 @@
 
 PORT="${PORT:-6379}"
 
-sed -i "s/__PORT__/$PORT/g" /usr/local/etc/redis.conf
-sed -i "s/__REQUIREPASS__/$REDIS_PASSWORD/g" /usr/local/etc/redis.conf
-sed -i "s/__PRIMARYAUTH__/$REDIS_MASTER_PASSWORD/g" /usr/local/etc/redis.conf
+# Perform the sed operations on the file in /tmp
+sed -i "s/__PORT__/$PORT/g" /tmp/redis.conf
+sed -i "s/__REQUIREPASS__/$REDIS_PASSWORD/g" /tmp/redis.conf
+sed -i "s/__PRIMARYAUTH__/$REDIS_MASTER_PASSWORD/g" /tmp/redis.conf
 
 exec "$@"
